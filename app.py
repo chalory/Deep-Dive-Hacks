d080ea1ee4785a1f3e3195fa9e2c9cd6adb2f2b1
rvitality <vital.renzjohn@gmail.com>
2022-10-23 16:11:49 +0800

    feat: add endpoints

--- app.py
+++ app.py
@@ -8,27 +8,43 @@ from sklearn.preprocessing import StandardScaler
 from flask import Flask, request, render_template
 from model import *
 
-co = cohere.Client('gqVPQNIEYu4Dei3YOaopVg9xwUyWU1VDD7tMBEyq')
+co = cohere.Client("gqVPQNIEYu4Dei3YOaopVg9xwUyWU1VDD7tMBEyq")
 app = Flask(__name__)
 
-@app.route('/', methods =["GET", "POST"])
-def gfg():
+final = ""
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
+def index():
     if request.method == "POST":
-       input = request.form.get("fname")
-       
-       
-       inputs=[input, "disgusting", "excited"]
-       print(type(inputs))
-       embeddings_train = co.embed(texts=inputs,
-                             model="large",
-                             truncate="LEFT").embeddings
-       score = svm_classifier.predict(embeddings_train)
-       final = str(score)
+        jsonData = request.get_json()
+        print(jsonData)
 
+        userResponse = jsonData["userResponse"]
+        print(userResponse)
 
-    #    response = co.classify(model='medium',inputs=inputs,examples=examples)
-    #    g=str(response)
-       print(score)
-    #    result = response.classifications
-       return final
+        inputs = [userResponse, "disgusting", "excited"]
+        print(inputs)
+
+        # test = ["I like turtles", "They are nice", "good"]
+
+        embeddings_train = co.embed(
+            texts=inputs, model="large", truncate="LEFT"
+        ).embeddings
+
+        score = svm_classifier.predict(embeddings_train)
+        final = str(score)
+
+        #    response = co.classify(model='medium',inputs=inputs,examples=examples)
+        #    g=str(response)
+        #    result = response.classifications
+        # return final
+
+        print(score)
+        return {"final": final}
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
